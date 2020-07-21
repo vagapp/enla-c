7d833ee55e94f3bded22a878f7d436013ff51957
Juan Carlos Mandujano <juancarlos@Juans-iMac.local>
2020-07-21 15:42:22 -0500

    Ajustes para presentacion con gilead

--- src/app/termsconds/termsconds.page.ts
+++ src/app/termsconds/termsconds.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../api/user.service';
 import { ModalController } from '@ionic/angular';
+import { GlobalsService } from '../api/globals.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonService } from '../app/common.service';
 
 @Component({
   selector: 'app-termsconds',
@@ -11,15 +14,22 @@ export class TermscondsPage implements OnInit {
   contenido:string = '';
   constructor(
     public US: UserService,
-    private modalController: ModalController
+    public global: GlobalsService,
+    private modalController: ModalController,
+    public co: CommonService
   ) { }
 
   ngOnInit() {
+    this.global.showLoader();
     this.US.termsCond().subscribe(
       (res:any) => {
+        this.global.hideLoader();
         this.contenido = res.body[0].value;
-      }
-    );
+      },(err:HttpErrorResponse) => {
+        this.global.hideLoader();
+        this.co.presentAlert('Error','Ocurrio un error la pagina.', err.error.message);
+      });
+    
   }
 
   closeModal(){
